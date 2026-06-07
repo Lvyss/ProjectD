5d7a004afb51e033177b33e8ee21bb8b96ec9cfe
Thuathe <raifuteika15@gmail.com>
2026-06-07 22:39:51 +0700

    sidebar3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -313,7 +313,7 @@ const handleSelectTerritory = (
     className="stroke-img-mobile"
     src="/stroke1.png" 
     alt=""
-    style={{ width: '100%', height: 'auto', display: 'block' }}
+    style={{ width: '65', height: 'auto', display: 'block' }}
   />
 </div>
 
